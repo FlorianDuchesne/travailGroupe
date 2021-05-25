49a347173c175636d07e22a9a754e8d51755b603
JPGurak <71316871+JPGurak@users.noreply.github.com>
2021-05-25 14:08:59 +0200

    isGranted

--- src/Controller/ProgrammerController.php
+++ src/Controller/ProgrammerController.php
@@ -5,10 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 
 class ProgrammerController extends AbstractController
 {
     /**
+     * @IsGranted("ROLE_ADMIN")
      * @Route("/programmer", name="programmer")
      */
     public function index(): Response
