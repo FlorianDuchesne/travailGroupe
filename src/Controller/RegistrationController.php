2c3083baf25063d02fb28b9fec3b7e62ae073996
FlorianDuchesne <florian.duchesne@gmail.com>
2021-05-27 10:00:24 +0200

    fini crud user

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,22 +4,40 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
 {
+
+
     /**
-     * @IsGranted("ROLE_ADMIN")
-     * @Route("/register", name="app_register")
+     * @IsGranted("ROLE_USER")
+     * @Route("/{id}/delete", name="deleteProfil")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function delete(User $user, EntityManagerInterface $manager)
     {
-        $user = new User();
+        $manager->remove($user);
+        $manager->flush();
+        return $this->redirectToRoute('login');
+    }
+
+    /**
+     * @IsGranted("ROLE_USER")
+     * @Route("/register", name="app_register")
+     * @Route("/{id}/edit", name="editProfil")
+     */
+    public function register(Request $request, User $user = null, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        if (!$user) {
+            $user = new User();
+        }
+
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
@@ -42,6 +60,38 @@ class RegistrationController extends AbstractController
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
+            'editMode' => $user->getId() !== null
         ]);
     }
 }
+
+// // S'il n'y a pas de stagiaire lorsque la fonction est appelée (cas de la route stagiaire_add),
+// // On instancie un nouvel objet stagiaire
+// if (!$stagiaire) {
+//     $stagiaire = new Stagiaire();
+// }
+// // On crée un formulaire à partir de la classe StagiaireType et de l'objet stagiaire
+// // Si l'objet stagiaire vient d'être créé, le formulaire sera vide, sinon il intégrera 
+// //les informations du stagiaire
+// $form = $this->createForm(StagiaireType::class, $stagiaire);
+
+// // handleRequest sert à inspecter si le formulaire est soumis, et le cas échéant à appeler la requête
+// $form->handleRequest($request);
+// // Si le formulaire a été soumis et est valide…
+// if ($form->isSubmitted() && $form->isValid()) {
+//     // on assigne à l'objet stagiaire le data du formulaire
+//     $stagiaire = $form->getData();
+//     // On signale qu'on veut enregistrer l'objet stagiaire en base de données
+//     $manager->persist($stagiaire);
+//     // L'enregistrement s'effectue en base de données
+//     $manager->flush();
+//     // On se rend à la page dont le nom de route est 'stagiaire'
+//     return $this->redirectToRoute('stagiaire');
+// }
+// // La fonction renvoie la vue twig où se situe le formulaire
+// // Le formulaire créé est appelé formAddStagiaire
+// // 'editMode est créé si on a un id de stagiaire
+// return $this->render('stagiaire/add_edit.html.twig', [
+//     'formAddStagiaire' => $form->createView(),
+//     'editMode' => $stagiaire->getId() !== null
+// ]);
